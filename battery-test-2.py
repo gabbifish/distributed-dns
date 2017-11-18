2c4c652711568b85fba6cb15ea691c94d5fe7d02
Gabbi Fisher <gabbifisher@gmail.com>
2017-11-17 18:57:20 -0800

    version where battery-test sets, b-t-2/3 gets

--- battery-test-2.py
+++ battery-test-2.py
@@ -1,11 +1,35 @@
 from pysyncobj import SyncObj
+from pysyncobj import SyncObjConf
 from pysyncobj.batteries import ReplCounter, ReplDict
+import time
 
-counter1 = ReplCounter()
-counter2 = ReplCounter()
+def printKeys(dict1):
+    result1 = dict1.get("testKey1", None)
+    result2 = dict1.get("testKey2", None)
+
+    while result1 == None or result2 == None:
+        result1 = dict1.get("testKey1", None)
+        result2 = dict1.get("testKey2", None)
+
+    print "testKey1 : " + result1
+    print "testKey2 : " + result2
+
 dict1 = ReplDict()
-syncObj = SyncObj('127.0.0.1:4322', ['127.0.0.1:4321'], consumers=[counter1, counter2, dict1])
+config = SyncObjConf(appendEntriesUseBatch=True)
+syncObj = SyncObj('127.0.0.1:4322', ['127.0.0.1:4321', '127.0.0.1:4323'], consumers=[dict1], conf=config)
 
-dict1.set('testKey1', 'testValue1', sync=True)
-print(counter1, counter2, dict1.get('testKey1'), dict1.get('testKey2'))
+# dict1.set('testKey1', 'testValue1', sync=True)
+# print(syncObj.waitBinded())
+# print(syncObj.isReady())
 
+
+while not syncObj.isReady():
+    continue
+
+printKeys(dict1)
+
+
+
+# dict1['testKey1'] = 'testValue1'
+# print(dict1, dict1.get('testKey1'), dict1.get('testKey2'))
+#
--- battery-test-3.py
+++ battery-test-3.py
@@ -16,17 +16,10 @@ def printKeys(dict1):
 
 dict1 = ReplDict()
 config = SyncObjConf(appendEntriesUseBatch=True)
-syncObj = SyncObj('127.0.0.1:4321', ['127.0.0.1:4322', '127.0.0.1:4323'], consumers=[dict1], conf=config)
+syncObj = SyncObj('127.0.0.1:4323', ['127.0.0.1:4321', '127.0.0.1:4322'], consumers=[dict1], conf=config)
 
 
 while not syncObj.isReady():
     continue
-
-print "About to set"
-dict1.set('testKey1', 'testValue1', sync=True)
-dict1.set('testKey2', 'testValue2', sync=True)
-
-while True:
-    time.sleep(1)
 
 printKeys(dict1)
